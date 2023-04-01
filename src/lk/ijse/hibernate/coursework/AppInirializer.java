d17d229262edd4fc46dabdb1a5085c964fb23b6d
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-04-01 19:21:46 +0530

    navigation aded

--- src/lk/ijse/hibernate/coursework/AppInirializer.java
+++ src/lk/ijse/hibernate/coursework/AppInirializer.java
@@ -1,4 +1,26 @@
 package lk.ijse.hibernate.coursework;
 
-public class AppInirializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInirializer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource
+                ("/lk/ijse/hibernate/coursework/view/MainMenuForm.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Hostel Management System");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
 }
--- src/lk/ijse/hibernate/coursework/controller/MainMenuFormController.java
+++ src/lk/ijse/hibernate/coursework/controller/MainMenuFormController.java
@@ -1,4 +1,37 @@
 package lk.ijse.hibernate.coursework.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 public class MainMenuFormController {
+    public AnchorPane MyPane;
+    public JFXButton btn;
+
+    private void setNode(Node node) {
+        MyPane.getChildren().clear();
+        MyPane.getChildren().add(node);
+    }
+    public void onClikUser(MouseEvent mouseEvent) throws IOException {
+        Node node = FXMLLoader.load(getClass().getResource("../view/UserForm.fxml"));
+        setNode(node);
+    }
+
+    public void onClikStudent(MouseEvent mouseEvent) throws IOException {
+        Node node = FXMLLoader.load(getClass().getResource("../view/StudentForm.fxml"));
+        setNode(node);
+    }
+
+    public void onClikRoom(MouseEvent mouseEvent) throws IOException {
+        Node node = FXMLLoader.load(getClass().getResource("../view/RoomForm.fxml"));
+        setNode(node);
+    }
+
+    public void onClikReservation(MouseEvent mouseEvent) throws IOException {
+        Node node = FXMLLoader.load(getClass().getResource("../view/ReservationForm.fxml"));
+        setNode(node);
+    }
 }
